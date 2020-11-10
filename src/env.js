ce2f2e61aa1a74fe21e17fb8386d1740a43b661a
roy1210 <rswkure@gmail.com>
2020-11-10 22:28:46 +0800

    fix: Make https://beptool-fork.vercel.app as mainnet

--- src/env.js
+++ src/env.js
@@ -1,21 +1,16 @@
 const prod_hostnames = [
-  "beptools.org",
-  "binancetools-prod.firebaseapp.com",
-  "binancetools-prod.web.app",
-  "localhost",
-]
+  'beptools.org',
+  'binancetools-prod.firebaseapp.com',
+  'binancetools-prod.web.app',
+  'localhost',
+  'beptool-fork.vercel.app',
+];
 
-const isMainnet = prod_hostnames.includes(window.location.hostname)
-const isTestnet = !isMainnet
-const CHAIN_ID = isTestnet ? "Binance-Chain-Nile" : "Binance-Chain-Tigris"
-const NETWORK_ID = 714
+const isMainnet = prod_hostnames.includes(window.location.hostname);
+const isTestnet = !isMainnet;
+const CHAIN_ID = isTestnet ? 'Binance-Chain-Nile' : 'Binance-Chain-Tigris';
+const NETWORK_ID = 714;
 
-const NET = isTestnet ? "testnet" : "mainnet"
+const NET = isTestnet ? 'testnet' : 'mainnet';
 
-export {
-  NET,
-  CHAIN_ID,
-  NETWORK_ID,
-  isTestnet,
-  isMainnet,
-}
+export { NET, CHAIN_ID, NETWORK_ID, isTestnet, isMainnet };
